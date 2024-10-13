dc4f618aa83cb11e9cd1f12d714cc33e5e9e53b3
Peace44 <peacebonerabose@gmail.com>
2024-10-13 18:25:24 +0200

    ...

---
+++ script.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('img');
+    images.forEach(img => {
+        img.setAttribute('loading', 'lazy');
+    });
+});
+
+function toggleMenu() {
+    const header = document.querySelector('header');
+    const menuToggle = document.querySelector('.menu-toggle');
+
+    header.classList.toggle('show-nav');
+
+    // Toggle the menu icon between '☰' and '✕'
+    menuToggle.innerHTML = header.classList.contains('show-nav') ? '✕' : '☰';
+}
+
+// Close the menu when a link is clicked
+document.querySelectorAll('header nav a').forEach(link => {
+    link.addEventListener('click', () => {
+        const header = document.querySelector('header');
+        const menuToggle = document.querySelector('.menu-toggle');
+
+        header.classList.remove('show-nav'); // Hide the menu after clicking
+        menuToggle.innerHTML = '☰';  // Reset to menu icon
+    });
+});
+
+function scrollToSection(sectionId) {
+    const offset = document.querySelector('header').offsetHeight;
+    const element = document.getElementById(sectionId);
+    const elementPosition = element.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - offset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
+
+function highlightCurrentSection(sectionId) {
+    const navLinks = document.querySelectorAll('header nav a');
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${sectionId}`) {
+            link.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const offset = document.querySelector('header').offsetHeight + 50;
+    let currentSectionId = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - offset;
+        if (window.scrollY >= sectionTop) {
+            currentSectionId = section.getAttribute('id');
+        }
+    });
+
+    highlightCurrentSection(currentSectionId);
+});
+
+let container = document.querySelector('.swipe-container');
+let daySection = document.querySelector('.day-section');
+let daySectionWidth = daySection.offsetWidth;
+
+window.addEventListener('resize', () => {
+    daySectionWidth = daySection.offsetWidth;
+});
+
+function scrollProgramLeft() {
+    container.scrollBy({ left: -daySectionWidth, behavior: 'smooth' });
+}
+
+function scrollProgramRight() {
+    container.scrollBy({ left: daySectionWidth, behavior: 'smooth' });
+}
